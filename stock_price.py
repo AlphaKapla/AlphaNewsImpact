19ea22477ab32cbc11bda5a5890c9e5b0eba0585
AlphaKapla <max.gau44@gmail.com>
2024-10-16 13:27:38 +0100

    Update stock_price.py
    give stock name in argument

--- stock_price.py
+++ stock_price.py
@@ -5,37 +5,53 @@ from ibapi.ticktype import TickTypeEnum
 from ibapi.common import *
 
 class StockPrice(EWrapper, EClient):
-    def __init__(self):
+    def __init__(self, sname: str):
         EClient.__init__(self, self)
-        # here you can add variables to the TestApp class, just use self.var in the class
-        self.last = 0;
+        self.last_price = 0
+        self.stock_name = sname
         
-    # ib suggests waiting for this response to know that you have a connection
+    def symbolSamples(self, reqId: int, contractDescriptions: ListOfContractDescription):
+        super().symbolSamples(reqId, contractDescriptions)
+        print("Symbol Samples. Request Id: ", reqId) 
+        for contractDescription in contractDescriptions:
+            derivSecTypes = ""
+            for derivSecType in contractDescription.derivativeSecTypes:
+                derivSecTypes += " "
+                derivSecTypes += derivSecType
+            print("Contract: conId:%s, symbol:%s, secType:%s primExchange:%s, "
+                  "currency:%s, derivativeSecTypes:%s" % (
+                contractDescription.contract.conId,
+                contractDescription.contract.symbol,
+                contractDescription.contract.secType,
+                contractDescription.contract.primaryExchange,
+                contractDescription.contract.currency, derivSecTypes))
+            self.disconnect() 
+
     def nextValidId(self, orderId:int):    
-        self.reqMarketDataType(MarketDataTypeEnum.REALTIME) # or DELAYED
+        self.reqMarketDataType(MarketDataTypeEnum.REALTIME) 
         contract = Contract()
-        contract.symbol = "AAPL"
+        contract.symbol = self.stock_name
         contract.secType = "STK"
         contract.currency = "USD"
         contract.exchange = "SMART"
         self.reqMktData(1, contract, "", False, False, None)
 
-    def error(self, reqId, errorCode, errorString):
+    def error(self, reqId: int, errorCode: int, errorString: str, advancedOrderRejectJson: str = ""):  # Add the extra argument
         print('Info: ', reqId, ' ', errorCode, ' ', errorString)
 
-    def tickPrice(self, reqId, tickType, price, attrib):
-            print('Tick Price. Ticker Id:', reqId, 'tickType:', TickTypeEnum.to_str(tickType), 
-            'Price:', price)
-            if tickType == TickTypeEnum.LAST or tickType == TickTypeEnum.DELAYED_LAST :
-                self.last = price;
-                print("disconnecting")
-                self.disconnect() # just for testing, normally the program would do something
+    def tickPrice(self, reqId: int, tickType: int, price: float, attrib: TickAttrib):
+        print('Tick Price. Ticker Id:', reqId, 'tickType:', TickTypeEnum.to_str(tickType), 
+              'Price:', price)
+        if tickType == TickTypeEnum.LAST or tickType == TickTypeEnum.DELAYED_LAST:
+            self.last_price = price
+            print("disconnecting")
+            self.disconnect() 
 
 def main():
-    app = StockPrice()
+    app = StockPrice("GWRE")  # Pass the stock name here
     app.connect('127.0.0.1', 7496, 123)
-    app.run() # this blocks the program until disconnect()
-    print("app.last:", app.last) # now you refer to the variable by class.var
+    app.run() 
+    print("app.last_price:", app.last_price) 
 
 if __name__ == '__main__':
     main()
